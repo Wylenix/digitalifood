b668de44a73135f09881b357421ca8c99d216a24
Lionel Auray <lionel.auray@gmail.com>
2024-05-14 15:39:12 +0200

    modification du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import digitalichef from "../../public/digitalichef.jpg";
 export default function Header() {
   const { user } = useUser();
+  console.log(user);
   return (
     <header className="flex w-full items-center py-4">
       <h1 className="inline-flex items-center gap-2 text-lg font-bold">
@@ -25,17 +26,20 @@ export default function Header() {
       </h1>
       <div className="flex gap-2 ml-auto">
         {user ? (
-          <Link href="/api/auth/logout">
-            {" "}
-            <User size={24} />
-          </Link>
+          <>
+            <p className="inline-flex items-center  justify-center   px-3 py-1.5 ">
+              Hello {user.nickname}
+            </p>
+            <Button variant="ghost">
+              <Link href="/api/auth/logout"> Se déconnecter</Link>
+            </Button>
+          </>
         ) : (
           <Link href="/api/auth/login">
             {" "}
             <User size={24} />
           </Link>
         )}{" "}
-        <Button variant="ghost"></Button>
       </div>
     </header>
   );
