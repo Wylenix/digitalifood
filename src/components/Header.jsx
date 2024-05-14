ed7ed4db7bb03c889b1deee4a5972eeb251fb0a4
Lionel Auray <lionel.auray@gmail.com>
2024-05-14 15:21:17 +0200

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,21 @@ import Button from "./Button";
 import { User } from "lucide-react";
 import Link from "next/link";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
+import digitalichef from "../../public/digitalichef.jpg";
 export default function Header() {
   const { user } = useUser();
   return (
     <header className="flex w-full items-center py-4">
       <h1 className="inline-flex items-center gap-2 text-lg font-bold">
-        <ReactSvg size="40" />
+        <Image
+          src={digitalichef}
+          width={50}
+          height={50}
+          alt="logo digitalifood"
+          priority="high"
+          fetchpriority="high"
+        />
         <Link href="/">
           {" "}
           <span>DigitaliFood</span>
